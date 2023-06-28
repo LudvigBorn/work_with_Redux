97d3f1e7eea9e1ba8054fab630bc4323041a14fb
Bralin Temirlan <ludvig.born@gmail.com>
2023-06-28 13:48:43 +0600

    195 Использовали useSelector и useDispatch это хуки необходимы для реализаци соединения в store с компонентом. Альтернатива connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,25 @@
 import { connect } from "react-redux";
-import * as actions from '../actions'
+import {inc,dec,rnd} from '../actions'
+import { useSelector, useDispatch } from "react-redux";
+
 //import { bindActionCreators } from "redux";
-const Counter = ({counter,inc,dec,rnd}) => {
+
+const Counter = () => {
+
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch();
+
   return (
     <>
       <div class="jumbotron">
         <h1>{counter}</h1>
-        <button onClick={dec} className="btn btn-btn-primary">
+        <button onClick={() => dispatch(dec())} className="btn btn-btn-primary">
           DEC
         </button>
-        <button onClick={inc} className="btn btn-btn-primary">
+        <button onClick={() => dispatch(inc())} className="btn btn-btn-primary">
           IMC
         </button>
-        <button onClick={rnd} className="btn btn-btn-primary">
+        <button onClick={() => dispatch(rnd())} className="btn btn-btn-primary">
           RND
         </button>
       </div>
@@ -20,12 +27,7 @@ const Counter = ({counter,inc,dec,rnd}) => {
     </>
   );
 }
-const mapStateToProps = (state) => {
-  return {
-    counter: state.value,
 
-  }
-}
 
 // const mapDispatchToProps = (dispatch) => {
 //   const {inc,dec,rnd} = bindActionCreators(actions,dispatch)
@@ -38,4 +40,11 @@ const mapStateToProps = (state) => {
 
 // export default connect(mapStateToProps,mapDispatchToProps)(Counter);
 
-export default connect(mapStateToProps, actions)(Counter);
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.value,
+//   };
+// };
+//export default connect(mapStateToProps, actions)(Counter);
+
+export default Counter;
